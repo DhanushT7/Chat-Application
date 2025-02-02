eb497c0ec7041b591768eca4a7a1853d53b0b86d
Jeffin Asir <jsjeffin0409@gmail.com>
2025-02-02 15:51:33 +0530

    initial commit2

--- Backend/server.js
+++ Backend/server.js
@@ -6,6 +6,6 @@ app.get("/", (req, res)=>{
   res.send("server is ready");
 });
 
-app.listen(5002, ()=>{
-  console.log("server started: http://localhost:5002");
+app.listen(5001, ()=>{
+  console.log("server started: http://localhost:5001");
 });
