f88c2d8fb47e5a48950af7e1e6182052138b7fbf
dhanush <tdhanush8124@gmail.com>
2025-02-07 21:58:07 +0530

    started main page

--- Frontend/src/pages/main/website.jsx
+++ Frontend/src/pages/main/website.jsx
@@ -1,8 +1,16 @@
+import './website.css'
 function Mainpage(){
   return(
-    <>
-      <h1>Main page</h1>
-    </>
+    <div className="main-page-body">
+      <div className="part1">
+        
+        <div className="title-box">
+          <p className="title">Gibberish</p>
+          <p className="title-msg1">Space to chat</p>
+        </div>
+
+      </div>
+    </div>
   );
 }
 
