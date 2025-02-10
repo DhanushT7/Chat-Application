3adf8682c2467acece589b2f463ca634c3903d81
Jeffin Asir <jsjeffin0409@gmail.com>
2025-02-10 19:22:25 +0530

    forgotPass page

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -3,7 +3,9 @@ import Mainpage from "./pages/main/website.jsx";
 import Home from './pages/home/Home.jsx'
 import Contact from './pages/contact/contact.jsx'
 import Login from './pages/login/login.jsx'
+import Forgot from "./pages/forgotPass/forgotPass.jsx"
 import Signup from "./pages/signup/signup.jsx";
+
 
 function App() {
   return(
@@ -12,6 +14,7 @@ function App() {
           <Route path="/" element={<Mainpage/>}/>
           <Route path="/home" element={ <Home/> } />
           <Route path="/login" element={ <Login/>} />
+          <Route path="/forgotPass" element={ <Forgot/> } />
           <Route path="/signup" element={ <Signup/> } />
           <Route path="/contact" element={ <Contact/> } />
         </Routes>
