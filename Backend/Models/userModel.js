e8b323d0096c901e3b469ac80bc5ebc03596f666
dhanush <tdhanush8124@gmail.com>
2025-04-07 21:53:15 +0530

    user model created

---
+++ Backend/Models/userModel.js
@@ -0,0 +1,34 @@
+import mongoose from "mongoose"
+
+const userSchema = mongoose.Schema(
+  {
+
+    name:{
+      type:String,
+      required:true,
+    },
+
+    email:{
+      type:String,
+      required:true,
+      unique:true,
+    },
+
+    password:{
+      type:String,
+      required:true,
+    },
+
+    picture:{
+      type:String,
+      required:true,
+      default:"https://icon-library.com/images/anonymous-avatar-icon/anonymous-avatar-icon-25.jpg"
+    }
+  },
+  {
+    timestamps:true,
+  }
+);
+
+const user = mongoose.model("User", userSchema);
+export default user;
